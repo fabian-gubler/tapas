eab1187571831baa24bc2adb36548e0b9ca10ad5
ronsei <ronny.seiger@unisg.ch>
2022-09-26 10:45:51 +0200

    Testing

--- app/src/main/java/ch/unisg/app/HelloController.java
+++ app/src/main/java/ch/unisg/app/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/app/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "Greetings from Spring Boot!!!";
     }
 
 }
