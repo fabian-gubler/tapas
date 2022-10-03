4dbc1e16f3243f39ce5ac191445ffa62b7b3b252
V4L3 <V4L3@users.noreply.github.com>
2022-10-03 16:34:09 +0200

    executor-joke microservice basic setup

---
+++ executor-joke/src/main/java/ch/unisg/executorjoke/ExecutorJokeController.java
@@ -0,0 +1,21 @@
+package ch.unisg.executorjoke;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class ExecutorJokeController {
+
+    private final String API_URI = "https://api.chucknorris.io/jokes/random";
+
+    @GetMapping("/executor-joke/")
+    public String index() {
+        return getJoke();
+    }
+
+    private String getJoke() {
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForObject(API_URI, String.class);
+    }
+}
